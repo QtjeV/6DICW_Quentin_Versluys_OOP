a0fda70e6f1758e23e6c7f77f685cded7d091633
QtjeV <quentin.versluys@sfevergem.be>
2025-09-18 12:24:57 +0200

    hi

---
+++ Oef1_Leeuw/Leeuw/Program.vb
@@ -0,0 +1,13 @@
+Imports System
+
+Module Program
+    Public Class Leeuw
+        Public intsnelheid As Integer
+        Public intleeftijd As Integer
+    End Class
+    Sub Main()
+        Dim objLeeuw As New Leeuw()
+        objLeeuw.intsnelheid = 10
+        objLeeuw.intleeftijd = 10
+    End Sub
+End Module
