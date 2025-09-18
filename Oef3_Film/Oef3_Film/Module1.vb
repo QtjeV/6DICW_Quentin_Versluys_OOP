4ec46b2d91cb5370155d46855065429dfbcdf732
QtjeV <quentin.versluys@sfevergem.be>
2025-09-18 12:47:11 +0200

    a

---
+++ Oef3_Film/Oef3_Film/Module1.vb
@@ -0,0 +1,13 @@
+﻿Imports System
+
+Module Program
+    Public Class Film
+        Public stringtitel As String
+        Public intjaar As Integer
+        Public intDuurvandefilm As Integer
+        Public booleanactiefilm As Boolean
+    End Class
+    Sub Main()
+        Dim Film As New Film()
+    End Sub
+End Module
---
+++ Oef3_Film/Oef3_Film/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Oef3_Film")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Oef3_Film")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("405ce6b5-b208-4e5f-8719-53ae39ab35ed")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
