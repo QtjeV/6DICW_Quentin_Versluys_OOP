62f7ef8f859db3a77c4e6625f2e4493aec247ab0
QtjeV <quentin.versluys@sfevergem.be>
2025-09-18 12:38:55 +0200

    oef :D

---
+++ Oef2_Rijbewijs/Oef2_Rijbewijs/Program.vb
@@ -0,0 +1,8 @@
+Module Program
+    Class Rijbewijs
+        Public HeeftRijbewijs As Boolean
+        Public Sub New(heeft As Boolean)
+            HeeftRijbewijs = heeft
+        End Sub
+    End Class
+End Module
