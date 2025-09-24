6eda1669c797a2e9c89c0330bc892ac2e9a8236a
QtjeV <quentin.versluys@sfevergem.be>
2025-09-24 15:01:33 +0200

    Draaken

---
+++ H4_Oefeningen/Oef1_Draak/Oef1_Draak/Module1.vb
@@ -0,0 +1,33 @@
+﻿Module Module1
+    ' Klasse
+    Public Class Draak
+        ' Velden 
+        Public Property Naam As String
+        Public Property Origin As String
+        Public Property Gender As String
+        Public Property Leeftijd As Integer
+        Public Property Classificatie As String
+
+        ' Constructor 
+        Public Sub New(naam As String, origin As String, gender As String, leeftijd As Integer, classificatie As String)
+            Me.Naam = naam
+            Me.Origin = origin
+            Me.Gender = gender
+            Me.Leeftijd = leeftijd
+            Me.Classificatie = classificatie
+        End Sub
+    End Class
+
+    Sub Main()
+        ' Objecten aanmaken van draken
+        Dim draak1 As New Draak("Drogon", "Essos", "Mannelijk", 7, "Vuurdraak")
+        Dim draak2 As New Draak("Rhaegal", "Essos", "Mannelijk", 7, "Vuurdraak")
+        Dim draak3 As New Draak("Viserion", "Essos", "Mannelijk", 6, "Vuurdraak")
+
+        ' print draken
+        Console.WriteLine("Draak 1: " & draak1.Naam & ", " & draak1.Origin & ", " & draak1.Gender & ", " & draak1.Leeftijd & " jaar, " & draak1.Classificatie)
+        Console.WriteLine("Draak 2: " & draak2.Naam & ", " & draak2.Origin & ", " & draak2.Gender & ", " & draak2.Leeftijd & " jaar, " & draak2.Classificatie)
+        Console.WriteLine("Draak 3: " & draak3.Naam & ", " & draak3.Origin & ", " & draak3.Gender & ", " & draak3.Leeftijd & " jaar, " & draak3.Classificatie)
+        Console.ReadLine()
+    End Sub
+End Module
---
+++ H4_Oefeningen/Oef1_Draak/Oef1_Draak/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Oef1_Draak")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Oef1_Draak")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("50095b3d-3a32-4e6c-b6ea-b5c108a508f9")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
