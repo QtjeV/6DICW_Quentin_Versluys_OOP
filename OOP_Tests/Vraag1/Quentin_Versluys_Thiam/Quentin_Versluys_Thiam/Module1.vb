4833962cecd0042703a2c6470ee12483335dcc4e
QtjeV <quentin.versluys@sfevergem.be>
2025-10-02 12:35:13 +0200

    Hierbij de oefeningen voor de toets (hopelijk 10/10)

---
+++ OOP_Tests/Vraag1/Quentin_Versluys_Thiam/Quentin_Versluys_Thiam/Module1.vb
@@ -0,0 +1,33 @@
+﻿Module Module1
+    Public Class Atleet
+        Public geboorteplaats As String
+        Public lengteInCm As Integer
+        Public gewichtInKg As Integer
+
+        Public Sub New(geboorteplaats As String, lengteInCm As Integer, gewichtInKg As Integer)
+            Me.geboorteplaats = geboorteplaats
+            Me.lengteInCm = lengteInCm
+            Me.gewichtInKg = gewichtInKg
+        End Sub
+
+        Public Function getgeboorteplaats()
+            Return "Thiam is geboren in : " & geboorteplaats
+        End Function
+        Public Function getlengteInCm()
+            Return "Thiam haar lengte (in Cm) is : " & lengteInCm
+        End Function
+
+        Public Function getgewichtInKg()
+            Return "Thiam haar gewicht (in Kg) is : " & gewichtInKg
+        End Function
+    End Class
+    Sub Main()
+        Dim objThiam As New Atleet("Gent", 180, 77)
+        Console.WriteLine("---")
+        Console.WriteLine(objThiam.getgeboorteplaats())
+        Console.WriteLine(objThiam.getlengteInCm())
+        Console.WriteLine(objThiam.getgewichtInKg())
+        Console.WriteLine("---")
+    End Sub
+
+End Module
---
+++ OOP_Tests/Vraag2/Versluys_Quentin_Dier/Versluys_Quentin_Dier/Module1.vb
@@ -0,0 +1,41 @@
+﻿Module Module1
+    Public Class Dier
+        Public naam As String
+        Public leeftijd As Integer
+        Public aanwezig As Boolean
+        Public gewicht As Double
+
+        Public Sub New(naam As String, leeftijd As Integer, aanwezig As Boolean, gewicht As Double)
+            Me.naam = naam
+            Me.leeftijd = leeftijd
+            Me.aanwezig = aanwezig
+            Me.gewicht = gewicht
+        End Sub
+
+        Public Sub ZetAanwezig(status)
+            aanwezig = status
+        End Sub
+
+        Public Function GeefAanwezig()
+            If aanwezig Then
+                Return True
+            Else
+                Return False
+            End If
+        End Function
+
+        Public Function GeefNaam()
+            Return naam
+        End Function
+    End Class
+    Sub Main()
+        Dim objDier As New Dier("Joske de Leeuw", 12, False, 280.2)
+        objDier.ZetAanwezig(True)
+        If objDier.GeefAanwezig() Then
+            Console.WriteLine("Dier met naam: " & objDier.GeefNaam() & " is aanwezig")
+        Else
+            Console.WriteLine("Dier met naam: " & objDier.GeefNaam() & " is afwezig")
+        End If
+    End Sub
+
+End Module
