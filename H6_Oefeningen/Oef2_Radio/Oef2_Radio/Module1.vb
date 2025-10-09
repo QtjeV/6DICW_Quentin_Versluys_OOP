83a852d2ebc98e00c0c8fe8cfd5164937baa7944
QtjeV <quentin.versluys@sfevergem.be>
2025-10-09 12:25:46 +0200

    oef inkapsuling met private

---
+++ H6_Oefeningen/Oef2_Radio/Oef2_Radio/Module1.vb
@@ -0,0 +1,23 @@
+﻿Imports System
+Public Class Radio
+    Private rnd As New Random()
+    Private liedjes() As String
+    Private indexliedje As Integer
+
+    Public Sub SpeelLiedje()
+        SpeelLiedjeRadio()
+    End Sub
+    Private Sub SpeelLiedjeRadio()
+        liedjes = {"Imagine Dragons", "Coldplay", "Despacito"}
+        indexliedje = rnd.Next(liedjes.Length)
+        Console.WriteLine("Ik ga een liedje spelen...")
+        Console.WriteLine(liedjes(indexliedje))
+    End Sub
+End Class
+Module Module1
+    Sub Main()
+        Dim mijnRadio As New Radio()
+        mijnRadio.SpeelLiedje()
+    End Sub
+End Module
+
