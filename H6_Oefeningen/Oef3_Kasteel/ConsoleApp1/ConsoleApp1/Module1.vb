e8038879953d3b03c7fa87bb2fe9e880b481a28a
QtjeV <quentin.versluys@sfevergem.be>
2025-10-16 12:46:00 +0200

    kasteel

---
+++ H6_Oefeningen/Oef3_Kasteel/ConsoleApp1/ConsoleApp1/Module1.vb
@@ -0,0 +1,45 @@
+﻿Module Module1
+    Class Kasteel
+        ' Velden
+        Private strKoning As String
+        Private intAantalRidders As Integer
+
+        ' Constructor
+        Public Sub New()
+            NaamKoning()
+        End Sub
+        'Maakt een random naam voor de koning door het te vergelijken met een andere naam en op basis van lengte de langste te kiezen.
+        'bepaalt het aantal ridders op basis van de lengte van de naam
+        Private Sub NaamKoning()
+            Dim random As New Random()
+            Dim namen As String() = {"Arthur", "Lodewijk", "Richard", "Filips", "Karel", "Sir Bazinga de Viertiende van Langebeke"}
+            Dim naam1 As String = namen(random.Next(namen.Length))
+            Dim naam2 As String = namen(random.Next(namen.Length))
+            If Len(naam1) > Len(naam2) Then
+                strKoning = naam1
+            Else
+                strKoning = naam2
+            End If
+
+            intAantalRidders = Len(WielsKoning())
+
+        End Sub
+
+        'Geeft de naam van de koning terug
+        Public Function WielsKoning() As String
+            Return strKoning
+        End Function
+        ' Geeft het aantal ridders terug
+        Public Function GeefAantalRidders() As Integer
+            Return intAantalRidders
+        End Function
+    End Class
+    Sub Main()
+        ' Maak een instantie van de Kasteel klasse
+        Dim objKasteel As New Kasteel()
+        ' Toon de naam van de koning en het aantal ridders
+        Console.WriteLine("De koning is " & objKasteel.WielsKoning())
+        Console.WriteLine("Aantal ridders: " & objKasteel.GeefAantalRidders())
+    End Sub
+
+End Module
---
+++ H6_Oefeningen/Oef_Keeper/ConsoleApp1/ConsoleApp1/Module1.vb
@@ -0,0 +1,41 @@
+﻿Module Module1
+    Public Class Kieper
+        Private strVoorkeur As String
+
+        Public Sub New()
+            strVoorkeur = "midden"
+        End Sub
+
+
+
+        Public Sub ZetVoorkeurNietRandom(Voorkeur As String)
+            Dim rnd As New Random()
+            Dim keuze As Integer = rnd.Next(1, 4)
+            If keuze = 1 Then
+                strVoorkeur = "Links"
+            ElseIf keuze = 2 Then
+                strVoorkeur = "Midden"
+            ElseIf keuze = 3 Then
+                strVoorkeur = "Rechts"
+            End If
+
+        End Sub
+
+
+
+
+
+
+
+        Public Function GeefVoorkeur() As String
+            Return strVoorkeur
+        End Function
+    End Class
+    Sub Main(args As String())
+        Dim Sheldon As New Kieper()
+
+        Sheldon.ZetVoorkeurNietRandom("links")
+
+        Console.WriteLine(Sheldon.GeefVoorkeur())
+    End Sub
+End Module
